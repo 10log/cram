66a91229b0a9bee71946f36b4f03b73d84dd604c
Rob Miller <rdmiller@gmail.com>
2025-12-04 17:09:48 -0600

    chore: update react-window dependency to version ^2.2.3 and refactor MaterialsPanel component

--- src/components/parameter-config/MaterialsPanel.tsx
+++ src/components/parameter-config/MaterialsPanel.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { VariableSizeGrid as Grid } from "react-window";
+import React, { CSSProperties } from 'react';
+import { Grid } from "react-window";
 import Messenger from "../../messenger";
 
 export interface MaterialsPanelProps{
@@ -10,24 +10,35 @@ const columnWidths = [
   100,200,200,500,100,100,100,100,100,100
 ]
 
+type CellProps = {
+  materials: Record<string, unknown>[];
+  keys: string[];
+}
+
+const Cell = ({ columnIndex, rowIndex, style, materials, keys }: {
+  columnIndex: number;
+  rowIndex: number;
+  style: CSSProperties;
+  materials: Record<string, unknown>[];
+  keys: string[];
+}) => (
+  <div style={style}>
+    {JSON.stringify(materials[rowIndex][keys[columnIndex]])}
+  </div>
+);
+
 export default function MaterialsPanel(props: MaterialsPanelProps) {
   const materials = props.messenger?.postMessage("FETCH_ALL_MATERIALS")[0]
-  
+
   const keys = Object.keys(materials[0]);
 
-  const Cell = ({ columnIndex, rowIndex, style }: { columnIndex: number, rowIndex: number, style: any }) => (
-    <div style={style}>
-      {JSON.stringify(materials[rowIndex][keys[columnIndex]])}
-    </div>
-  );
-  
-  return (<Grid
+  return (<Grid<CellProps>
+    cellComponent={Cell}
+    cellProps={{ materials, keys }}
     columnCount={5}
-    columnWidth={_index => columnWidths[_index]}
+    columnWidth={(_index) => columnWidths[_index]}
     rowCount={500}
-    rowHeight={_index => 50}
-    height={500}
-    width={800}>
-    {Cell}
-  </Grid>);
+    rowHeight={() => 50}
+    style={{ height: 500, width: 800 }}
+  />);
 }
